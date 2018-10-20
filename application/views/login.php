c189ba62cc3fe8d8f44cda8b5d162882868b4f4a
zhangshuang-h <781792980@qq.com>
2018-10-20 17:55:43 +0800

    login php

---
+++ application/views/login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登陆</title>
+    <base href="<?php echo site_url()?>">
+</head>
+<body>
+<form action="welcome/login_check" method="post">
+    <p>
+        用户名:<input type="text" name="username">
+    </p>
+    <p>
+        密码:<input type="password" name="password">
+    </p>
+    <p>
+        <input type="submit" value="登陆">
+    </p>
+</form>
+</body>
+</html>
